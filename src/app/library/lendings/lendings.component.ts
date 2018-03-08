741865df55120ff0c3e1451dff56de3df1c2ce1e
Tamar Mayost <tamarm909@gmail.com>
2018-03-08 18:08:07 +0200

    final version of the project

--- src/app/library/lendings/lendings.component.ts
+++ src/app/library/lendings/lendings.component.ts
@@ -12,24 +12,58 @@ export class LendingsComponent implements OnInit {
   private currentLend : LendingViewModel = new LendingViewModel();
   private massage : String ;
   
+  private errorMessage:string="";
+  private error:boolean=true;
+  private successMessage:string="";
+  private success:boolean=true;
+
   constructor(private lendingService:LendingService) { }
 
   ngOnInit() {
     this.massage="";
+    this.errorMessage = "";
+    this.successMessage="";
   }
 
   addLend() {
-    this.lendingService.insertLoan(this.currentLend).then(result=>{
+    this.error=true;
+    this.success=true;
+    this.errorMessage = "";
+    this.successMessage="";
+    if(!this.currentLend.readerId){
+      this.error=false;
+      this.errorMessage = "please enter reader ID";
+    }else if(!this.currentLend.bookId){
+      this.error=false;
+      this.errorMessage = "please enter book ID";
+    }else{this.lendingService.insertLoan(this.currentLend).then(result=>{
       debugger;
       if(result){ debugger;
         switch(result){
-          case 0:this.massage="the book was loaned successfully";break;
-          case 1:this.massage="This reader doesn't exist in our library";break;
-          case 2:this.massage="You can't loan now. You have too many books";break;
-          case 3:this.massage="You can't loan now. this book is loaned already";break;
+          case 0:
+          //this.massage="the book was loaned successfully";
+          this.success=false;
+          this.successMessage="the book was loaned successfully";
+          break;
+          case 1:
+          //this.massage="This reader doesn't exist in our library";
+          this.error=false;
+          this.errorMessage ="This reader doesn't exist in our library";
+          break;
+          case 2:
+          //this.massage="You can't loan now. You have too many books";
+          this.error=false;
+          this.errorMessage ="You can't loan now. You have too many books";
+          break;
+          case 3:
+          //this.massage="You can't loan now. this book is loaned already";
+          this.error=false;
+          this.errorMessage ="You can't loan now. this book is loaned already";
+          break;
         }
       }
     });
   }
+}
 
 }
--- src/app/library/returnings/returnings.component.ts
+++ src/app/library/returnings/returnings.component.ts
@@ -11,14 +11,31 @@ export class ReturningsComponent implements OnInit {
   private currentReturn:LendingViewModel = new LendingViewModel();
   private loanExists:boolean;
   private massage : String;
+  private errorMessage:string="";
+  private error:boolean=true;
+  private successMessage:string="";
+  private success:boolean=true;
 
   constructor(private lendingService:LendingService) { }
 
   ngOnInit() {
     this.massage = "";
+    this.errorMessage = "";
+    this.successMessage="";
   }
 
   returnBook(){
+    this.error=true;
+    this.success=true;
+    this.errorMessage = "";
+    this.successMessage="";
+    if(!this.currentReturn.readerId){
+      this.error=false;
+      this.errorMessage = "please enter reader ID";
+    }else if(!this.currentReturn.bookId){
+      this.error=false;
+      this.errorMessage = "please enter book ID";
+    }else{
    //this.loanExists = this.lendingService.updateReturn(this.currentReturn);
    this.lendingService.updateReturn(this.currentReturn).then(result=>{
     debugger;
@@ -26,11 +43,16 @@ export class ReturningsComponent implements OnInit {
       debugger;
      if(result){
       debugger;
-        this.massage = "the book was retured successfully";
+        //this.massage = "the book was retured successfully";
+        this.success=false;
+        this.successMessage="the book was retured successfully";
       }else{
-        this.massage = "couldn't return the book as expected";
+        //this.massage = "couldn't return the book as expected";
+        this.error=false;
+        this.errorMessage ="couldn't return the book as expected";
       }
     }
   });
   }
+}
 }
